5f93ea8724854d4fb9683e4ea17c2fbe6fdbc970
JiHyeonMon <jhk8806@naver.com>
2020-07-21 03:00:55 +0900

    [소켓통신] Revise, Security, Reset Activity - 완료

--- app/src/main/java/com/entersekt/fido2/ResetActivity.kt
+++ app/src/main/java/com/entersekt/fido2/ResetActivity.kt
@@ -3,8 +3,20 @@ package com.entersekt.fido2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_reset.*
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.net.Socket
 
 class ResetActivity : AppCompatActivity() {
+
+    companion object{
+        var socket = Socket()
+        lateinit var writeSocket: DataOutputStream
+        lateinit var readSocket: DataInputStream
+        var ip = "192.168.1.167"  //서버 ip
+        var port = 9999
+        var msg = "0"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +24,33 @@ class ResetActivity : AppCompatActivity() {
 
         btn_back.setOnClickListener {
             finish()
+        }
+
+        act_reset_btn_restart.setOnClickListener {
+            Connect(1).start()
+        }
+
+        act_reset_btn_reset.setOnClickListener {
+            Connect(0).start()
+        }
+    }
+
+    class Connect(i: Int) :Thread(){
+        val con = i
+        override fun run(){
+            try{
+                SecurityActivity.socket = Socket(SecurityActivity.ip, SecurityActivity.port)
+                SecurityActivity.writeSocket = DataOutputStream(SecurityActivity.socket.getOutputStream())
+                SecurityActivity.readSocket = DataInputStream(SecurityActivity.socket.getInputStream())
+
+                SecurityActivity.msg = if(con == 1){"restart"}else "reset"
+
+                SecurityActivity.writeSocket.write(SecurityActivity.msg.toByteArray())    //메시지 전송 명령 전송
+
+            }catch(e:Exception){    //연결 실패
+                SecurityActivity.socket.close()
+            }
+
         }
     }
 }
--- app/src/main/java/com/entersekt/fido2/ReviseActivity.kt
+++ app/src/main/java/com/entersekt/fido2/ReviseActivity.kt
@@ -8,9 +8,23 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_host.btn_back
 import kotlinx.android.synthetic.main.activity_revise.*
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.net.Socket
 
 class ReviseActivity : AppCompatActivity() {
 
+    companion object{
+        var socket = Socket()
+        lateinit var writeSocket: DataOutputStream
+        lateinit var readSocket: DataInputStream
+        var ip = "192.168.1.167"  //서버 ip
+        var port = 9999
+        var msg = "0"
+    }
+
+    var username = ""
+    var pwd = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,17 +41,14 @@ class ReviseActivity : AppCompatActivity() {
                 Toast.makeText(this, "아이디 또는 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
             }
             else{
-                if(txt_pwd.text.toString().equals(txt_repwd.text.toString())){
-//                    inputMethodManager.hideSoftInputFromWindow(txt_repwd.getWindowToken(), 0);
+                if(txt_pwd.text.toString() == txt_repwd.text.toString()){
+                    username = txt_newUserName.text.toString()
+                    pwd = txt_pwd.text.toString()
 
-//                    txt_userName.setText(txt_newUserName.text)
-//                    Toast.makeText(this, "변경 완료", Toast.LENGTH_SHORT).show()
-//                    txt_newUserName.setText("")
-//                    txt_pwd.setText("")
-//                    txt_repwd.setText("")
+                    Connect(username, pwd).start()
 
                     val intent = Intent(this, InformationActivity::class.java)
-                    intent.putExtra("username", txt_newUserName.text.toString())
+                    intent.putExtra("username", username)
                     startActivity(intent)
                     finish()
 
@@ -45,6 +56,26 @@ class ReviseActivity : AppCompatActivity() {
                     Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+    }
+
+    class Connect(username: String, pwd: String) :Thread(){
+        val username = username
+        val pwd = pwd
+        override fun run(){
+            try{
+                socket = Socket(ip, port)
+                writeSocket = DataOutputStream(socket.getOutputStream())
+                readSocket = DataInputStream(socket.getInputStream())
+
+                msg = "${username}% ${pwd}"
+
+                writeSocket.write(msg.toByteArray())    //메시지 전송 명령 전송
+
+            }catch(e:Exception){    //연결 실패
+                SecurityActivity.socket.close()
+            }
+
         }
     }
 }
