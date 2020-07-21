0c0cc3b5d2330ede6647a2c4b7d168c7a044f91c
helena <l1004ga@gmail.com>
2020-07-22 03:15:59 +0900

    #보안기능 알림 내용 작성 #첫 등록시 등록 버튼 아니콘 수정 #sign in-up 아이콘 수정

--- app/src/main/java/com/entersekt/fido2/fragment_log/LogAdapter.kt
+++ app/src/main/java/com/entersekt/fido2/fragment_log/LogAdapter.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.entersekt.fido2.fragment_log.active.ActiveFragment
 import com.entersekt.fido2.fragment_log.security.SecurityFragment
+import kotlinx.android.synthetic.main.activity_log.*
 
 class LogAdapter(fm: FragmentManager):
         FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
     override fun getItem(position: Int): Fragment {
         return when(position){
-            0 -> SecurityFragment()
+            0 -> SecurityFragment(
+
+            )
             else -> ActiveFragment()
         }
     }
+
 
     override fun getCount() = 2
 
